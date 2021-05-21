628d1eb33cb98c4f1127b84438c4be6dc3433972
danfigueroa <danielmfigueroa@gmail.com>
2021-05-21 14:33:44 -0300

    Init do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.green,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.green,
         ),
         body: DicePage(),
       ),
